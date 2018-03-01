d7c81485709e2a9b63cc9b5077049f697eccfef1
Keyu Lin <mlchaellin222@gmail.com>
2018-02-28 20:43:09 -0500

    fixed redux store connect problem

--- my-reddit/src/App.js
+++ my-reddit/src/App.js
@@ -1,34 +1,28 @@
 import React, { Component } from 'react';
-
 import './App.css';
-import { Provider } from 'react-redux';
-
-
 import Search from './components/Search';
-
-import store from './components/store';
-
 import styled from 'styled-components';
+import { connect } from 'react-redux';
 
 const bdiv=styled.div`
   background-image: url("https://unsplash.com/photos/kO1G3neRA2o");
 
 `;
 
+
+
 class App extends Component {
   constructor(props) {
     super(props);
     this.updateContents=this.updateContents.bind(this);
     this.cutText=this.cutText.bind(this);
+    this.getImgSrc=this.getImgSrc.bind(this);
   }
   componentDidMount(){
-    store.subscribe(()=>{
-      console.log("changed store");
-      this.updateContents();
-    })
+
   }
   updateContents(){
-    var arr=store.getState().arr;
+    var arr=this.props.arr;
     var grid='<div>';
     arr.forEach(item =>{
       var imgsrc=item.preview ? item.preview.images[0].source.url : 'https://source.unsplash.com/weekly?water';
@@ -48,22 +42,42 @@ class App extends Component {
     }
     return text;
   }
+  getImgSrc(item){
+
+    console.log(item);
+    return  'https://source.unsplash.com/weekly?water';
+  }
   render() {
+
     return (
-      <Provider store={store}>
         <bdiv className="App">
             <nav class="navbar bg-info text-white navbar-static-top">
               <a class="navbar-brand text-white" href="/">Daily Reddit</a>
             </nav>
             <div class="container">
               <Search />
-              <div id="grid"  ref='grid' class="col-sm-12 pt-5"/>
+              <div id="grid"  ref='grid' class="col-sm-12 pt-5">
+                {
+                  this.props.arr.map(item =>
+                      <div class="container col-sm-4 bg-light">
+                        <img class="img-thumbnail" src={item.preview ? item.preview.images[0].source.url.replace("&amp;","&") : 'https://source.unsplash.com/weekly?water'} />
+                        <h4 class="card-title">{this.cutText(item.title)}</h4>
+                        <hr />
+                      </div>
+                  )
+                }
+              </div>
             </div>
-
         </bdiv>
-      </Provider>
+
     );
   }
 }
 
-export default App;
+function mapStateToProps(state) {
+  return {
+    arr: state.arr
+  }
+}
+
+export default connect(mapStateToProps)(App);
--- my-reddit/src/components/store.js
+++ my-reddit/src/components/store.js
@@ -2,5 +2,5 @@
 import { createStore } from 'redux';
 import updateStore from '../reducers/updateStore';
 
-const store = createStore(updateStore,{arr:[]});
+const store = createStore(updateStore,{arr:[]},window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 export default store;
